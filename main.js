1088ed5ee78cb567e9945ee072f4c7c740cb24f0
dwseoh127 <82667749+dwseoh127@users.noreply.github.com>
2023-07-13 12:28:16 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function removepop() {
+    var elem = document.getElementById('popup');
+    elem.parentNode.removeChild(elem);
+    var elem2 = document.getElementById('button-6');
+    elem2.parentNode.removeChild(elem2);
+  
+
+}
+
